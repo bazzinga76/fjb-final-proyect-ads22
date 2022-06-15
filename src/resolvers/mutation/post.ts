5658d82527334c1a116f43bcced9e5e8d4b7b2a8
Filiberto Juárez <juarezfilb@globalhitss.com>
2022-06-15 17:23:30 -0500

    fix - resolver mutation post

--- src/resolvers/mutation/post.ts
+++ src/resolvers/mutation/post.ts
@@ -1,51 +1,33 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import {
-    Resolver,
-    Query,
-    Ctx,
-    InputType,
-    Field,
-    Mutation,
-    Arg,
-} from 'type-graphql'
-import { User } from '../../db/entities'
-import { Post } from '../../db/entities'
-import { Context } from '../../config/context'
-
-
-@InputType()
-class PostCreateInput {
-    @Field()
-    title: string;
-
-    @Field()
-    content: string;
-
-    @Field()
-    published: boolean;
-
-    @Field()
-    authorId: number;
-}
-
+  Resolver,
+  Query,
+  Ctx,
+  InputType,
+  Field,
+  Mutation,
+  Arg,
+} from "type-graphql";
+import { Prisma } from "@prisma/client";
+import { User } from "../../db/entities";
+import { Post } from "../../db/entities";
+import { Context } from "../../config/context";
 
 @Resolver(Post)
 export class PostMutation {
-
-    @Mutation((returns) => Post)
-    async signupUser(
-        @Arg('data') data: PostCreateInput,
-        @Ctx() ctx: Context
-    ): Promise<Post> {
-        return ctx.prisma.post.create({
-            data: {
-                title: data.title,
-                name: data.content,
-                published: data.published,
-                authorId: data.authorId,
-                viewCount: data.viewCount
-            }
-        })
-    }
-    
+  @Mutation((returns) => Post)
+  async signupUser(
+    @Arg("data") data: Prisma.PostCreateInput,
+    @Ctx() ctx: Context
+  ): Promise<Post> {
+    return ctx.prisma.post.create({
+      data: {
+        title: data.title,
+        name: data.content,
+        published: data.published,
+        authorId: data.authorId,
+        viewCount: data.viewCount,
+      },
+    });
+  }
 }
