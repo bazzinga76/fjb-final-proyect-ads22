90359445c98cbbb94dd01aea788ad3d855312aab
Filiberto Juárez <juarezfilb@globalhitss.com>
2022-06-15 21:38:54 -0500

    reactions

--- src/resolvers/query/reaction.ts
+++ src/resolvers/query/reaction.ts
@@ -1,20 +1,12 @@
-import 'reflect-metadata';
-import {
-    Resolver,
-    Query,
-    Ctx,
-    InputType,
-    Field,
-} from 'type-graphql'
-import { Reaction } from '../../db/entities'
-import { Context } from '../../config/context'
+import "reflect-metadata";
+import { Resolver, Query, Ctx, InputType, Field } from "type-graphql";
+import { Reaction } from "../../db/entities";
+import { Context } from "../../config/context";
 
 @Resolver(Reaction)
 export class ReactionQuery {
-
-    @Query(() => [Reaction])
-    async allReactions(@Ctx() ctx: Context) {
-        return ctx.prisma.reaction.findMany()
-    }
-    
+  @Query(() => [Reaction])
+  async allReactions(@Ctx() ctx: Context) {
+    return ctx.prisma.reaction.findMany();
+  }
 }
