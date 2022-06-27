a83614f7d01fc9d67982c794613b8943e307c0fd
Filiberto Juárez <juarezfilb@globalhitss.com>
2022-06-27 11:18:07 -0500

    add - script.ts

--- script.ts
+++ script.ts
@@ -1,25 +1,43 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: [
+    { level: "warn", emit: "event" },
+    { level: "info", emit: "event" },
+    { level: "error", emit: "event" },
+  ],
+});
+
+prisma.$on("warn", (e) => {
+  console.log(e);
+});
+
+prisma.$on("info", (e) => {
+  console.log(e);
+});
+
+prisma.$on("error", (e) => {
+  console.log(e);
+});
 
 // A `main` function so that you can use async/await
 async function main() {
   // ... you will write your Prisma Client queries here
   const allUsers = await prisma.user.findMany({
     include: { posts: true },
-  })
+  });
 
-  const allPosts = await prisma.post.findMany({
-    include: { posts: true}, 
-  })
+  const countUsers = await prisma.user.count({});
+
   // use `console.dir` to print nested objects
-  console.dir(allUsers, { depth: null })
+  console.dir(countUsers, { depth: null });
+  console.dir(allUsers, { depth: null });
 }
 
 main()
-  .catch(e => {
-    throw e
+  .catch((e) => {
+    throw e;
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
