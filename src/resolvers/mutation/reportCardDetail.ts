5c98e518ea895a87b51e15335abfbb7f0cc9fb1d
Filiberto Juárez <juarezfilb@globalhitss.com>
2022-06-27 19:46:52 -0500

    add - env - authSource

--- src/resolvers/mutation/reportCardDetail.ts
+++ src/resolvers/mutation/reportCardDetail.ts
@@ -11,3 +11,38 @@ import {
 import { Prisma } from "@prisma/client";
 import { ReportCardDetail } from "../../db/entities";
 import { Context } from "../../config/context";
+
+@Resolver(ReportCardDetail)
+export class ReportCardDetailMutation {
+  @Mutation((returns) => ReportCardDetail)
+  async createReportCardDetail(
+    @Arg("data") data: Prisma.ReportCardDetailCreateInput,
+    @Ctx() ctx: Context
+  ): Promise<ReportCardDetail> {
+    return ctx.prisma.reportCardDetail.create({
+      data,
+    });
+  }
+
+  @Mutation(() => [ReportCardDetail])
+  async updateReportCardDetail(
+    @Arg("data") data: Prisma.ReportCardDetailUpdateArgs,
+    @Ctx() ctx: Context,
+    params: { reportcarddetailId: string }
+  ) {
+    return ctx.prisma.reportCardDetail.update({
+      where: { id: params.reportcarddetailId },
+      data,
+    });
+  }
+
+  @Mutation(() => [ReportCardDetail])
+  async deleteReportCardDetail(
+    @Ctx() ctx: Context,
+    params: { reportcarddetailId: string }
+  ) {
+    return ctx.prisma.reportcarddetails.delete({
+      where: { id: params.reportcarddetailId },
+    });
+  }
+}
